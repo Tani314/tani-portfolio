818108a74026873db9d5bbef1ae4394471bb0a0e
Tani314 <tanitabassum299@gmail.com>
2020-11-19 03:20:04 -0500

    build

---
+++ build/precache-manifest.4fddff953701de597e2d1b21587d9311.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faeb0db421e2886d4e655b008882ea3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a76cf69f046ce0d80d4",
+    "url": "/static/css/main.898a3045.chunk.css"
+  },
+  {
+    "revision": "4e62037ac5ea4c507680",
+    "url": "/static/js/2.d6b70270.chunk.js"
+  },
+  {
+    "revision": "5e9b84942a6ee25128dd8950a9f79626",
+    "url": "/static/js/2.d6b70270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a76cf69f046ce0d80d4",
+    "url": "/static/js/main.a955f1f5.chunk.js"
+  },
+  {
+    "revision": "7439d9a7c475f55bbb1a",
+    "url": "/static/js/runtime-main.9135dee9.js"
+  },
+  {
+    "revision": "fc5d04eb05144c6e5140ed05c217525f",
+    "url": "/static/media/Armor.fc5d04eb.gif"
+  },
+  {
+    "revision": "d5d667bdd0395bb74c65ddd61e036ffd",
+    "url": "/static/media/Exit.d5d667bd.jpg"
+  },
+  {
+    "revision": "c42db85dab4220074ceb41aa3e89492e",
+    "url": "/static/media/Get-Tested.c42db85d.jpg"
+  },
+  {
+    "revision": "00221c296ccd9a4533505d6ddf5ad398",
+    "url": "/static/media/GraceShopper.00221c29.jpg"
+  },
+  {
+    "revision": "baf38dc102a5d3ff786c51c0ca65b13f",
+    "url": "/static/media/IMG_2877.baf38dc1.jpeg"
+  },
+  {
+    "revision": "d39b280286915ae5597442b007fff0e6",
+    "url": "/static/media/Pizza.d39b2802.jpg"
+  },
+  {
+    "revision": "ea92d6580dda0fbcf61a8ccb16e0d02a",
+    "url": "/static/media/Postgres.ea92d658.png"
+  },
+  {
+    "revision": "d6d2ebb60dc64e11bb1c135d747aff36",
+    "url": "/static/media/REST.d6d2ebb6.png"
+  },
+  {
+    "revision": "68fcf38c8c8e178799686d15abef5944",
+    "url": "/static/media/bootstrap.68fcf38c.png"
+  },
+  {
+    "revision": "f6108efd045ebcd489345bc22964bb86",
+    "url": "/static/media/chai.f6108efd.png"
+  },
+  {
+    "revision": "8316e471d267b903f454ec34e1100296",
+    "url": "/static/media/jasmine.8316e471.png"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/static/media/react.876a8325.svg"
+  }
+]);
