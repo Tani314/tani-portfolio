2b3faecb63468f5db53e3188562b489b84cfbc4b
Nuzhat Tabassum Tani <ntani@Nuzhats-MacBook-Pro-2.local>
2024-09-18 22:22:19 -0400

    new project added

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -12,6 +12,7 @@ import {
   ImageColumn,
   ImgP,
 } from "../styles/portfolio";
+import Task from "../assets/Task.png";
 import Pizza from "../assets/Pizza.jpg";
 import GraceShopper from "../assets/GraceShopper.jpg";
 import GetTested from "../assets/Get-Tested.jpg";
@@ -27,12 +28,30 @@ const Portfolio = (props) => {
           <HR />
           <ImageColumn>
             <ImageContain>
+              <Image src={Task} />
+              <ImageMid>
+                <ImgText>Task Manager</ImgText>
+                <ImgP>
+                  Task Manager is a web application designed to streamline task
+                  management. It enables users to efficiently organize, track,
+                  and monitor the progress of their tasks, providing a
+                  comprehensive overview of their productivity and project
+                  advancements.
+                </ImgP>
+                <a href="https://github.com/Tani314/Task-Manager">
+                  <Button>View Code</Button>
+                </a>
+              </ImageMid>
+            </ImageContain>
+            <ImageContain>
               <Image src={Pizza} />
               <ImageMid>
                 <ImgText>PizzaNow</ImgText>
                 <ImgP>
-                  Pizza NOW is web application that allows users to report
-                  domestic violence, disguised as a pizza delivery app
+                  Pizza NOW is a web application designed to provide a discreet
+                  platform for reporting domestic violence. It cleverly
+                  disguises itself as a pizza delivery app, offering a safe and
+                  confidential means for users to seek help.
                 </ImgP>
                 <a href="https://github.com/pizza-now/earthxhack20">
                   <Button>View Code</Button>
@@ -46,10 +65,11 @@ const Portfolio = (props) => {
               <ImageMid>
                 <ImgText>Exit</ImgText>
                 <ImgP>
-                  Exit is an escape room game compatible with web, mobile and VR
-                  platforms that allows users to explore the room by enabling
-                  camera movement in 360 environment, search for and solve clues
-                  by interacting with the objects to escape the room.
+                  Exit is an immersive escape room game compatible with web,
+                  mobile, and VR platforms. It allows players to explore a fully
+                  interactive 360-degree environment, search for and solve
+                  clues, and interact with objects to ultimately escape the
+                  room.
                 </ImgP>
 
                 <a href="https://stormlooper.github.io/Exit/">
@@ -63,8 +83,10 @@ const Portfolio = (props) => {
               <ImageMid>
                 <ImgText>GraceShopper</ImgText>
                 <ImgP>
-                  GraceShopper is an e-commerce website where guests and user
-                  can purchase groceries
+                  GraceShopper is an intuitive e-commerce website where both
+                  guests and registered users can easily browse and purchase
+                  groceries. It offers a seamless shopping experience with a
+                  user-friendly interface.
                 </ImgP>
                 <a href="https://github.com/Captain-Thunk-Grace-Shopper/Grace-Shopper">
                   <Button>View Code</Button>
@@ -77,9 +99,10 @@ const Portfolio = (props) => {
               <ImageMid>
                 <ImgText>Armor-Wing</ImgText>
                 <ImgP>
-                  Native mobile app renders dragon in augmented reality and
-                  allows users to interact with the dragon to trigger
-                  animations.
+                  Armor-Wing is a native mobile app that brings a dragon to life
+                  through augmented reality. Users can interact with the dragon
+                  to trigger various animations and experiences, enhancing their
+                  engagement with the virtual creature.
                 </ImgP>
                 <a href="https://github.com/Tani314/Armor-Wing">
                   <Button>View Code</Button>
@@ -92,8 +115,10 @@ const Portfolio = (props) => {
               <ImageMid>
                 <ImgText>Get-Tested</ImgText>
                 <ImgP>
-                  Get Tested COVID-19 is an open-source application which allows
-                  users to search for nearby Covid testing centers
+                  Get Tested COVID-19 is an open-source application designed to
+                  help users locate nearby COVID-19 testing centers. It provides
+                  up-to-date information on testing locations to facilitate easy
+                  access to testing services.
                 </ImgP>
                 <a href="https://get-tested-covid19.org/">
                   <Button>View Code</Button>
